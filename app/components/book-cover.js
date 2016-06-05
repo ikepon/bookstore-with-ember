d9150d48e25fc683f97b2953aa3b410a282246c6
ikepon <tatsuyanoheya@gmail.com>
2016-06-05 23:03:10 +0900

    modal が表示された時の背景に blur をかける #2

--- app/components/book-cover.js
+++ app/components/book-cover.js
@@ -4,10 +4,12 @@ export default Ember.Component.extend({
   actions: {
     open() {
       this.set('isShowingModal', true);
+      this.get('blurBackground')(true);
     },
 
     close() {
       this.set('isShowingModal', false);
+      this.get('blurBackground')(false);
     }
   }
 });
